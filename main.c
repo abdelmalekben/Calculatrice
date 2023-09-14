7f68751b8a98461d54acd6f446ba7733961eb133
[hello world] <[b.benabdelmalek94@gmail.com]>
2023-09-14 14:17:44 +0100

    calculatrice v.1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ main() {
+ 	int x,y;
+ 	char Char;
+ 	printf("1:(+)\n2:(-)\n3:(*)\n4:(/)\nEnter a choice:");
+ 	scanf("%c",&Char);
+ 	printf("enter first numbre:");
+ 	scanf("%d",&x);
+ 	printf("enter scond numbre:");
+ 	scanf("%d",&y);
+ 	
+ 	
+ 	
+	return 0;
+}
